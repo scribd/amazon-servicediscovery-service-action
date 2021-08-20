263bbb97aa367995aea467ace5a9d86be60dcd3e
jim80net <jim80net@users.noreply.github.com>
2021-08-20 18:48:23 +0000

    :robot: yarn lint [skip ci]

--- release.config.js
+++ release.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  "branches": ['+([0-9])?(.{+([0-9]),x}).x', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+  'branches': ['+([0-9])?(.{+([0-9]),x}).x', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
+  'plugins': [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
-      "@semantic-release/changelog",
+      '@semantic-release/changelog',
       {
-        "changelogFile": "CHANGELOG.md"
-      }
+        'changelogFile': 'CHANGELOG.md',
+      },
     ],
     [
-      "@semantic-release/git",
+      '@semantic-release/git',
       {
-        "assets": [
-          "CHANGELOG.md"
+        'assets': [
+          'CHANGELOG.md',
         ],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
+        'message': 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
     ],
-  ]
+  ],
 };
